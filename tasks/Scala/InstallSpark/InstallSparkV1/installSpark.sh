70860a57dac202a2a2582e233c9c3e1567a6cc4f
FGiBiT <44163065+FGiBiT@users.noreply.github.com>
2021-03-13 11:00:42 +0100

    Update installSpark.sh

--- tasks/Scala/InstallSpark/InstallSparkV1/installSpark.sh
+++ tasks/Scala/InstallSpark/InstallSparkV1/installSpark.sh
@@ -18,7 +18,7 @@ setupSpark() {
    SPARKURL=https://ftpmirror1.infania.net/mirror/apache/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz
    wget "$SPARKURL"
    tar xzvf spark-3.1.1-bin-hadoop2.7.tgz
-   PATH=/home/vsts/work/1/s/Databricks-pyspark/Databricks-pyspark
-   echo "Path = /home/vsts/work/1/s/Databricks-pyspark/Databricks-pyspark"
+   PATH=$PATH:/home/vsts/spark-3.1.1-bin-hadoop2.7/bin
+   echo "Path = "$PATH
 }
 setupSpark
